4fff101c36763d4725a3c0f380247450075698a0
Bahram <bahram.ashrafi@gmail.com>
2020-12-22 20:23:16 +0330

    [IMP] Position In Gem and Objects

---
+++ scripts/objects.js
@@ -0,0 +1,13 @@
+function Objects(){
+
+}
+Objects.prototype.spaceship = function(x,y,spaceship_image){
+    this.x = x;
+    this.y = y;
+    this.width = 41;
+    this.height = 48;
+    this.spaceship_image = spaceship_image;
+    this.spaceship_image.src = "images/ship.png";
+    return this;
+
+}
---
+++ scripts/positionInGame.js
@@ -0,0 +1,24 @@
+function InGamePosition(settings, level){
+    this.settings = settings;
+    this.level = level;
+    this.object = null;
+    this.spaceship = null;
+}
+InGamePosition.prototype.update = function(play){
+
+}
+
+InGamePosition.prototype.entry = function(play){
+    this.spaceship_image = new Image();
+    this.object = new Objects();
+    this.spaceship = this.object.spaceship((play.width / 2), play.playBoundaries.bottom, this.spaceship_image)
+}
+
+InGamePosition.prototype.keyDown = function(play){
+
+}
+
+InGamePosition.prototype.draw = function(play){
+    ctx.clearRect(0, 0, play.width, play.height);
+    ctx.drawImage(this.spaceship_image, this.spaceship.x - (this.spaceship.width/2), this.spaceship.y - (this.spaceship.height/2))
+}
